f2cd322474482391cc9af990fb7e313ca70aac30
John Pitts <jgpitts@gmail.com>
2019-08-15 20:19:22 -0400

    Added all icons for app, from Allegiance Twitter page using icon app.  Got to first stage of implementing Test Flight using proper bundle identifier which required jtaylor on the end.

--- AllegiancePractise/AllegiancePractise/MODELS/Group.swift
+++ AllegiancePractise/AllegiancePractise/MODELS/Group.swift
@@ -6,26 +6,26 @@
 //  Copyright © 2019 johnpitts. All rights reserved.
 //
 
-import Foundation
+//import Foundation
+//
+//class Group {
+//
+//    let id: Int
+//    var name: String // group_name
+//    var privacySetting: String
+//    var timestamp: Date
+//    var location: Int  // zipcode
+//    var creatorID: Int           // should be of type User?
+//
+//    var logo: String
+//
+//
+//
+//    var users: [User]
 
-class Group {
-    
-    let id: Int
-    var name: String // group_name
-    var privacySetting: String
-    var timestamp: Date
-    var location: Int  // zipcode
-    var creatorID: Int           // should be of type User?
-    
-    var logo: String
-    
-    
-    
-    var users: [User]
-    
 //    init(name: String, logo: Data) {
 //
 //        self.name = name
 //        self.logo = UIImage(data: data)
-    
-}
+//    
+//}
