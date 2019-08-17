7c914d51838fe991843209599fff90552f031c6a
John Pitts <jgpitts@gmail.com>
2019-08-16 22:09:17 -0400

    Added 4 Sign In View Controllers with black background and white script matching themes givin in Figma. Connected 4 views to respective cocoa touch files.  Began process of creating a swipe left and swipe right navigation to these views, but haven't added logic to app-delegate to distinguish which storyboard to open at startup so it still opens tab bar controller first

---
+++ AllegiancePractise/AllegiancePractise/MODELS/Auth ViewControllers/FindFansSignup2VC.swift
@@ -0,0 +1,30 @@
+//
+//  FindFansSignup2VC.swift
+//  AllegiancePractise
+//
+//  Created by John Pitts on 8/16/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import UIKit
+
+class FindFansSignup2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AllegiancePractise/AllegiancePractise/MODELS/Auth ViewControllers/SupportTeamsSignup1VC.swift
@@ -0,0 +1,59 @@
+//
+//  SupportTeamsSignup1VC.swift
+//  AllegiancePractise
+//
+//  Created by John Pitts on 8/16/19.
+//  Copyright © 2019 johnpitts. All rights reserved.
+//
+
+import UIKit
+
+class SupportTeamsSignup1VC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        
+        leftSwipe.direction = .left
+        rightSwipe.direction = .right
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+    }
+    
+    @objc func handleSwipes(_ sender:UISwipeGestureRecognizer)
+    {
+        if (sender.direction == .left)
+        {
+            print("Swipe Left")
+            
+            // show the view from the right side
+        }
+        
+        if (sender.direction == .right)
+        {
+            print("Swipe Right")
+            
+            // show the view from the left side
+        }
+    }
+    
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
