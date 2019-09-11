4dc7e9062215bdbd0413f53e5da0e1a9737baccf
John Pitts <jgpitts@gmail.com>
2019-09-10 22:48:04 -0400

    changed tableview datasource to reflect the initial cell as a header.  Need to add data-- posts-- to make it appear

--- AllegiancePractise/AllegiancePractise/GROUPTableViewController.swift
+++ AllegiancePractise/AllegiancePractise/GROUPTableViewController.swift
@@ -22,10 +22,10 @@ class GROUPTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -34,7 +34,7 @@ class GROUPTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath)
 
         // Configure the cell...
 
